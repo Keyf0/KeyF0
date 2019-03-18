c29a2da9d937001f7d5c04267fad80a5fc738220
gatesgao <329052613@qq.com>
2019-03-18 18:22:44 +0800

    1

--- Source/ACTS/Actor/Components/KFScriptComponent.ts
+++ Source/ACTS/Actor/Components/KFScriptComponent.ts
@@ -6,4 +6,13 @@ export class KFScriptComponent extends KFComponentBase
 {
     public static Meta:KFDName
         = new KFDName("KFScriptComponent");
+
+    private _targetScripts;
+    private _scopeOnceScripts;
+    private _scopeKeepScripts;
+    private _keepScriptID:number;
+    private _scriptruning:boolean;
+    private _isEndingscope:boolean;
+
+
 }
